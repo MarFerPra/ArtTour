2605ecbecb2e08fd6e83c77faa973822a8a23d5d
MarFerPra <mfernandezpranno@gmail.com>
2017-06-10 16:25:37 +0200

    Initial commit, base application.

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Linking
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+export default class qrscanner extends Component {
+  onRead(e) {
+    Linking.openURL(e.data).catch(err => console.error('An error occurred', err))
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <QRCodeScanner onRead={this.onRead.bind(this)}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('qrscanner', () => qrscanner);
